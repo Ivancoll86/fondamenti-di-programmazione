37235f6365c348179385c0b48a362739638a7370
Ivancoll86 <ivan.busini@edu-its.it>
2025-01-15 17:10:06 +0000

    esercitazione

---
+++ hello/esercitazione.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+int main()
+(
+'char' Nome [20]
+char Cognome [20]
+
+
+printf("Nome: ");
+printf("Cognome: ");
+scanf("%s", "Nome");
+
+scanf("%s", "Cognome");
+printf("\n"Benvenuto, Nome Cognome!" \t%s\n\n\n");
+
+ printf("Grazie per aver usato il nostro programma.  
+Ti auguriamo Buone Feste
+   *
+  ***
+ *****
+*******
+  ***
+  
+ ");
+
